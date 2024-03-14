3dc589929bfc28d4d548df27b61996beacaccd8e
Kirill Lukyanov <kirs2000@mail.ru>
2024-03-14 16:53:25 +0300

    ProfileRestControllerTest added

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,8 +1,109 @@
 package com.javarush.jira.profile.internal.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javarush.jira.AbstractControllerTest;
+import com.javarush.jira.common.util.JsonUtil;
+import com.javarush.jira.profile.internal.ProfileRepository;
+import com.javarush.jira.profile.internal.model.Profile;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static com.javarush.jira.login.internal.web.UserTestData.*;
+import static com.javarush.jira.profile.internal.web.ProfileRestController.REST_URL;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
 class ProfileRestControllerTest extends AbstractControllerTest {
+
+    @Autowired
+    private ProfileRepository repository;
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void getAllUsersAuth() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void getAllUsersUnAuth() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void create() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getNewTo())))
+                .andExpect(status().isNoContent());
+        Profile profileToActual = repository.getExisted(USER_ID);
+        Profile profileToExpected = ProfileTestData.getNew(USER_ID);
+        ProfileTestData.PROFILE_MATCHER.assertMatch(profileToActual, profileToExpected);
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void update() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getUpdatedTo())))
+                .andExpect(status().isNoContent());
+        Profile profileToActual = repository.getExisted(USER_ID);
+        Profile profileToExpected = ProfileTestData.getUpdated(USER_ID);
+        ProfileTestData.PROFILE_MATCHER.assertMatch(profileToActual, profileToExpected);
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateInvalidTo() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getInvalidTo())))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateWithUnknownNotificationTo() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getWithUnknownNotificationTo())))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateWithUnknownContactTo() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getWithUnknownContactTo())))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateWithContactHtmlUnsafeTo() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getWithContactHtmlUnsafeTo())))
+                .andExpect(status().is4xxClientError());
+    }
 
 }
